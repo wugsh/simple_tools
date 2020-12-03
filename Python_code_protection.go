85597704196357d229ef2f8752978ac6fe5db855
Gaosheng Wu <wugsh15@gmail.com>
2020-12-03 23:52:48 +0800

    Using Go protect Python code

--- Python_code_protection.go
+++ Python_code_protection.go
@@ -3,7 +3,7 @@ package main
 /*
 * Using Go binary file and encryption method to prevent Python code leakage.
 * Encrypting Python files --> Decrypting Python files --> Runing Python files --> Delete Decrypted Python files
-* Only Go binary and Decrypted Python files are left at run time and stop time.
+* Only Go binary and Encrypted Python files are left at run time and stop time.
 */
 
 import (
